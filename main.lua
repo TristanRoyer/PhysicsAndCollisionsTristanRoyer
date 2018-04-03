31c9ffcdb0b7d9269190a182d8f3e3836f9466e8
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-04-02 20:48:27 -0400

    ok

--- main.lua
+++ main.lua
@@ -15,4 +15,78 @@ physics.start()
 --Objects
 -------------------------------------------------------------------
 -- Ground
-local ground
+local ground = display.newImage("Physics/ground.png",0,0)
+
+--set the x and y pos
+ground.x = display.contentCenterX
+ground.y = display.contentHeight
+
+-- Change the width to be the same as the screen
+ground.width = display.contentWidth
+
+-- Add to Physics
+Physics.addBody(ground,"static", {friction=0.5, bounce=0.3})
+
+-- create a verticle beam
+local beam = display.newImage("Physics/beam.png", 0, 0)
+
+-- set the x and y pos
+beam.x = display.contentCenterX/5
+beam.y = display.contentCenterY*1.65
+
+--set the beam size
+beam.width = display.contentCenter/2
+beam.height = display.contentHeight/10
+
+--rotate the beam -60 degrees so its on an angle
+beam:rotate(45)
+
+--send it to the back layer
+beam:toBack()
+
+--add to physics
+physics.addBody(beam, "static", {friction=0.5,bounce=0.3})
+
+--create bkg
+local bkg = display.newImage("physics/bkg.png",0,0)
+
+-- set the x and y post
+bkg.x = display.contentCenterX
+bkg.y = display.contentCenterY
+
+-- set the bkg size
+bkg.width = display.contentWidth
+bkg.Height = display.contentHeight
+
+-- send to back
+bkg:toBack()
+
+---------------------------------
+--Functions
+---------------------------------
+
+-- create the first ball
+local function firstBall()
+	--creating first ball
+	local ball1 = display.newImage("Physics/super_ball.png",0,0)
+
+	--add to physics
+	physics.addBody(ball2, {density=1.0, friction=0.5,bounce=0.3,radius=25})
+end
+
+local function secondBall()
+	--creating second ball
+	local ball2 = display.newImage("Physics/super_ball.png",0,0)
+
+	--adding to physics
+	physics.addBody(ball2, {density=1.0, friction=0.5,bounce=0.3,radius=12.5})
+
+	--make it smaller than the original size
+	ball2:scale(0.5,0.5)
+end
+
+--------------------
+--Timer Delays - call each function after the given millisecond
+--------------------
+timer.performWithDelay(0,firstBall)
+timer.performWithDelay( 500, secondBall)
